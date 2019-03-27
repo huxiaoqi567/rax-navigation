22c5c67edd7d0dd8cbe86fac9c2ee1cc23264184
xiaoqi.huxq <xiaoqi.huxq@alibaba-inc.com>
2019-03-27 17:08:07 +0800

    refactor: remove comments

--- src/views/StatusBar.js
+++ src/views/StatusBar.js
@@ -1,26 +1,3 @@
-// TODO statusBar https://reactnavigation.org/docs/status-bar.html
-/*
-class Screen2 extends Component {
-  render() {
-    return (
-      <SafeAreaView style={[styles.container, { backgroundColor: '#ecf0f1' }]}>
-        <StatusBar
-          barStyle="dark-content"
-          backgroundColor="#ecf0f1"
-        />
-        <Text style={styles.paragraph}>
-          Dark Screen
-        </Text>
-        <Button
-          title="Next screen"
-          onPress={() => this.props.navigation.navigate('Screen1')}
-        />
-      </SafeAreaView>
-    );
-  }
-}
- */
-
 'use strict';
 
 import {createElement, Component} from 'rax';
